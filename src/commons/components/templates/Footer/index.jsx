076f3b9ba599233229a601320a56a780be01e359
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2022-04-05 19:46:58 -0300

    Footer - sample footer added

--- src/commons/components/templates/Footer/index.jsx
+++ src/commons/components/templates/Footer/index.jsx
@@ -1,7 +1,23 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: 70,
+        backgroundColor: "primary.main",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Typography align="center" variant="text">
+        Todos os direitos reservados
+      </Typography>
+    </Box>
+  );
 }
 
 export default Footer;
