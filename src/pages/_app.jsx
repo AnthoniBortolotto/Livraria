1b193eb4ece1c12258ee035994c1670ae6e9cc44
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2022-04-04 21:59:38 -0300

    Header - material example added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
+import Header from "../commons/components/templates/Header";
+import Footer from "../commons/components/templates/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
