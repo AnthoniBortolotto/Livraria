16977b0ee690c2abe543f4edc687b84aee8f9c4a
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2022-04-05 21:49:04 -0300

    Login - initial form added

--- src/commons/components/pages/Login/index.jsx
+++ src/commons/components/pages/Login/index.jsx
@@ -1,7 +1,39 @@
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Link from "next/link";
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Grid justifyContent="center" container>
+      <Grid item xs={6}>
+        <Box>
+          <Grid container justifyContent="center">
+            <Grid item xs={12}>
+              <TextField />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained">Login</Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>
+                Don&apos;t have an account?{" "}
+                <Link href="/signIn" passHref>
+                  <a>SignIn</a>
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Login;
