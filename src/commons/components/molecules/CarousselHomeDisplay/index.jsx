5cd501a1d588f8056e3b7c4acf658bb5617f3d8d
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2022-04-19 21:04:13 -0300

    Home - mobile responsiveness added temporarily

--- src/commons/components/molecules/CarousselHomeDisplay/index.jsx
+++ src/commons/components/molecules/CarousselHomeDisplay/index.jsx
@@ -1,20 +1,22 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 import React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { useRouter } from "next/router";
 import Caroussel from "../../atoms/Caroussel";
 
 const items = [
   {
-    img: "campeoes_venda.png",
+    img: "campeoes_venda",
     alt: "Clique aqui para acessar as ofertas dos campeões de venda",
     link: "https://www.saraiva.com.br/",
   },
   {
-    img: "desconto.png",
+    img: "desconto",
     alt: "Clique aqui para acessar os descontos",
     link: "https://www.saraiva.com.br/",
   },
   {
-    img: "dia_mundial_livro.png",
+    img: "dia_mundial_livro",
     alt: "Clique aqui para acessar as ofertas do dia mundial do livro",
     link: "https://www.saraiva.com.br/",
   },
@@ -22,14 +24,27 @@ const items = [
 
 function CarousselHomeDisplay() {
   const { push } = useRouter();
+  const isMobile = useMediaQuery("(max-width:600px)");
   return (
-    <Caroussel>
-      {items.map(({ alt, link, img }) => (
-        <div key={alt} onClick={() => push(link)}>
-          <img src={img} alt={alt} />
-        </div>
-      ))}
-    </Caroussel>
+    <>
+      {isMobile ? (
+        <Caroussel>
+          {items.map(({ alt, link, img }) => (
+            <div key={alt} onClick={() => push(link)}>
+              <img src={`${img}-Mob.png`} alt={alt} />
+            </div>
+          ))}
+        </Caroussel>
+      ) : (
+        <Caroussel>
+          {items.map(({ alt, link, img }) => (
+            <div key={alt} onClick={() => push(link)}>
+              <img src={`${img}.png`} alt={alt} />
+            </div>
+          ))}
+        </Caroussel>
+      )}
+    </>
   );
 }
 
