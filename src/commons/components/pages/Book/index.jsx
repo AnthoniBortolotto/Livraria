19d94acd6784a995a8a9420f4c05d03f99b3395f
DanielFBottega <daniel.bottega@unochapeco.edu.br>
2022-05-19 21:51:01 -0300

    estilizando pagina book

--- src/commons/components/pages/Book/index.jsx
+++ src/commons/components/pages/Book/index.jsx
@@ -1,9 +1,47 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import React, { useEffect } from "react";
+import styles from "./book.module.scss";
+import StarRatings from "react-star-ratings";
 
 export default function Book({ livro }) {
   useEffect(() => {
     console.log(livro);
   }, []);
 
-  return <div>Book</div>;
+  return (
+    <section className="default-margin containerDesk">
+      <Grid container direction="row" justifyContent="space-between">
+        <Grid item>
+          <Card className={styles.CardStyle}>
+            <CardContent>
+              <CardMedia
+                title={livro.title}
+                image={livro.img}
+                className={styles.CardMediaStyle}
+              />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Grid container direction="column" justifyContent="space-between">
+            <Grid item>
+              <Typography variant="h4" className={styles.title}>
+                {livro.title}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography className={styles.desc}>{livro.desc}</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </section>
+  );
 }
