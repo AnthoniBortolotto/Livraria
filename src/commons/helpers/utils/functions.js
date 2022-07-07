95813c1109b71e71150bf58a3ad119cd92f038c0
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2022-07-07 14:32:29 -0300

    Home - caroussel with different items

--- src/commons/helpers/utils/functions.js
+++ src/commons/helpers/utils/functions.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { domain } from "./global";
+
 export function treatTitle(title, width) {
   let treatedTitle = title;
   if (treatedTitle.length > 40) {
@@ -23,12 +26,109 @@ export function scrollTo(id) {
 }
 
 export function searchDescubra(search, items) {
-  const results =[]
+  const results = [];
   items.map((item, index) => {
-    if (item.title.toLowerCase().includes(search.toLowerCase()) || item.creator.toLowerCase().includes(search.toLowerCase()) || item.genres.toLowerCase().includes(search.toLowerCase())) {
+    if (
+      item.title.toLowerCase().includes(search.toLowerCase()) ||
+      item.creator.toLowerCase().includes(search.toLowerCase()) ||
+      item.genres.toLowerCase().includes(search.toLowerCase())
+    ) {
       results.push(item);
     }
-    });
+  });
 
   return results;
+}
+
+export async function getAllBooks() {
+  const itemList = [];
+  const authorPauloC = [];
+  const popular = [];
+  const highlights = [];
+  // run all the requests at same time
+  const [books, itensPauloC, itensPopular, itensHighlights] = await axios.all([
+    axios.get(`${domain}/livros/get`),
+    axios.get(`${domain}/livros/get/Paulo Coelho`),
+    axios.get(`${domain}/livro/get/livro/rate/4`),
+    axios.get(`${domain}/livro/get/livro/genre/fiction`),
+  ]);
+  books.data.map(({ img, author, genre, rating, title, isbn }, i) => {
+    itemList.push({
+      title,
+      creator: author,
+      imgUrl: img,
+      link: `/books/${isbn}`,
+      genres: genre,
+      rank: parseFloat(rating),
+    });
+  });
+
+  itensPauloC.data.map(({ img, author, genre, rating, title, isbn }, i) => {
+    authorPauloC.push({
+      title,
+      creator: author,
+      imgUrl: img,
+      link: `/books/${isbn}`,
+      genres: genre,
+      rank: parseFloat(rating),
+    });
+  });
+
+  itensPopular.data.map(({ img, author, genre, rating, title, isbn }, i) => {
+    popular.push({
+      title,
+      creator: author,
+      imgUrl: img,
+      link: `/books/${isbn}`,
+      genres: genre,
+      rank: parseFloat(rating),
+    });
+  });
+
+  itensHighlights.data.map(({ img, author, genre, rating, title, isbn }, i) => {
+    highlights.push({
+      title,
+      creator: author,
+      imgUrl: img,
+      link: `/books/${isbn}`,
+      genres: genre,
+      rank: parseFloat(rating),
+    });
+  });
+
+  return [itemList, authorPauloC, popular, highlights];
+}
+//join arrays of objects without duplicates
+export async function joinedBooks() {
+  const [itemList, authorPauloC, popular, highlights] = await getAllBooks(); //arrays of objects
+  //stringify arrays of objects
+  const stringifiedItemList = [];
+  itemList.map((item) => {
+    stringifiedItemList.push(JSON.stringify(item));
+  });
+  const stringifiedAuthorPauloC = [];
+  authorPauloC.map((item) => {
+    stringifiedAuthorPauloC.push(JSON.stringify(item));
+  });
+  const stringifiedPopular = [];
+  popular.map((item) => {
+    stringifiedPopular.push(JSON.stringify(item));
+  });
+  const stringifiedHighlights = [];
+  highlights.map((item) => {
+    stringifiedHighlights.push(JSON.stringify(item));
+  });
+  //concat arrays of stringified objects
+  const stringJoined = stringifiedItemList.concat(
+    stringifiedAuthorPauloC,
+    stringifiedPopular,
+    stringifiedHighlights
+  );
+  const unique = [...new Set(stringJoined)]; //remove duplicates
+  const uniqueBooks = [];
+  //parse stringified objects
+  unique.map((item) => {
+    uniqueBooks.push(JSON.parse(item));
+  });
+  return uniqueBooks;
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,55 +1,16 @@
 import Home from "../commons/components/pages/Home";
 import axios from "axios";
 import { domain } from "../commons/helpers/utils/global";
+import { getAllBooks } from "../commons/helpers/utils/functions";
 
 export const getStaticProps = async (ctx) => {
-  const item = {
-    title: "Teste",
-    rank: 4.22,
-    imgUrl:
-      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1373217358l/12841140.jpg",
-    link: "/",
-    creator: "Teste creator",
-  };
-
-  const itemList = [];
-
-  //  for (let i = 0; i < 20.length; i++) {
-  //    const element = 20[i];
-
-  //  }
-
-  const books = await axios.get(`${domain}/livros/get`);
-  books.data.map(({ img, author, genre, rating, title, isbn }, i) => {
-    itemList.push({
-      title,
-      creator: author,
-      imgUrl: img,
-      link: `/books/${isbn}`,
-      genres: genre,
-      rank: parseFloat(rating),
-    });
-  });
-
-  const authorPauloC = [];
-  const itensPauloC = await axios.get(`${domain}/livros/get/Paulo Coelho`);
-  itensPauloC.data.map(({ img, author, genre, rating, title, isbn }, i) => {
-    authorPauloC.push({
-      title,
-      creator: author,
-      imgUrl: img,
-      link: `/${isbn}`,
-      genres: genre,
-      rank: parseFloat(rating),
-    });
-  });
-
+  const [itemList, authorPauloC, popular, highlights] = await getAllBooks();
   return {
     props: {
       authorPauloC,
       itemList,
-      itemListPopular: itemList,
-      itemListHighlights: itemList,
+      itemListPopular: popular,
+      itemListHighlights: highlights,
     },
   };
 };
@@ -60,6 +21,7 @@ export default function Page({
   itemListHighlights,
   authorPauloC,
 }) {
+  
   return (
     <Home
       itemList={itemList}
