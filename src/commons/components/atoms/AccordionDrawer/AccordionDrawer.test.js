2fa55ec0028839f35e76de7f795a4bde68fc3438
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2022-09-09 21:09:41 -0300

    jest test working

---
+++ src/commons/components/atoms/AccordionDrawer/AccordionDrawer.test.js
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import AccordionDrawer from ".";
+
+describe("Test AccordionDrawer", () => {
+  it("Tests if the accordion is rendered correctly", () => {
+    const testText = "Oi tudo bem?";
+    const testTitle = "Oi titulo de teste";
+    const { getByText } = render(
+      <AccordionDrawer title={testTitle}>
+        <p>{testText}</p>
+      </AccordionDrawer>
+    );
+
+    const titulo = getByText(testTitle);
+    const texto = getByText(testText);
+
+    expect(titulo).toBeInTheDocument();
+    expect(texto).toBeInTheDocument();
+  });
+});
